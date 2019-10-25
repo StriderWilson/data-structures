e5bfdf50a7f5ecaaa79d42bd8e75bb2d95ec5d26
StriderWilson <strider.w@berkeley.edu>
2019-10-24 20:55:27 -0700

    Completes tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,54 @@
 var Tree = function(value) {
-  var newTree = {};
+  //parent obj
+  var newTree = Object.create(treeMethods);
+  //parent value
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
 
   return newTree;
 };
 
+
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  // create new obj
+  var newChild = Object.create(treeMethods);
+  // add properties
+  newChild.value = value;
+  newChild.children = [];
+  // find parent reference
+  this.children.push(newChild);
+
 };
 
 treeMethods.contains = function(target) {
+  // create recursive function
+  var result = false;
+
+  // node = object
+  var containsValue = function containsValue(node) {
+    // check in child value = target
+    if (node.value === target) {
+      result = true;
+    }
+    // iterate through the children array
+    if (node.children) {
+      for (var i = 0; i < node.children.length; i++) {
+        containsValue(node.children[i]);
+      }
+    }
+  };
+  // return recursive call to our function
+
+  // this = root
+  containsValue(this);
+
+  return result;
+
 };
 
 
