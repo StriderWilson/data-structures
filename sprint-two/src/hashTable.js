0f3407ca1285e7020f07f5b814eed7cf2a4b3cc0
StriderWilson <strider.w@berkeley.edu>
2019-10-25 19:34:32 -0700

    Completes hashTables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,39 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.getIndexBelowMaxForKey = getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  // add array to index of storage
+  this._storage[index] = [];
+  // create tuple
+  var tuple = [k, v];
+  // push tuple into array
+  this._storage[index].push(tuple);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  // iterate over array in index
+  for (var i = 0; i < this._storage[index].length; i++) {
+    // check if tuple contains value
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    // check if tuple contains value
+    if (this._storage[index][i][0] === k) {
+      this._storage[index][i].pop();
+    }
+  }
 };
 
 
